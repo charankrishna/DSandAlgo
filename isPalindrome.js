2100d444561e171e4fcc251c4b0e5b4dae5bcf73
charankrishna <charan.nithinj@gmail.com>
2018-07-17 22:38:13 +0530

    palindrome

---
+++ isPalindrome.js
@@ -0,0 +1,10 @@
+function palindrome(str){
+
+    let str1 = [...str].reverse().join('');
+    if(str===str1){
+        console.log(str+" is palindrom")}
+    else{
+        console.log(str+" Not palindrom")
+    }}
+
+palindrome("krupa")
